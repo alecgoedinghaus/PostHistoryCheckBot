3f25e59cbaa00fc0483ff6fc68175bb550ebb690
Alec Goedinghaus <alecgoedinghaus@ucla.edu>
2022-03-17 22:16:44 -0700

    general structure done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 # import requests
 import praw
+import argparse
 
 client_id = os.environ['PERSONAL_USE_SCRIPT']
 client_secret = os.environ['SECRET']
@@ -14,3 +15,31 @@ reddit = praw.Reddit(
     client_secret=client_secret,
     user_agent=user_agent
 )
+
+def generate_subreddits(user):
+    subreddits = dict()
+    
+    search_comments(user, subreddits)
+    search_posts(user, subreddits)
+    return
+
+
+def search_comments(user, subreddits):
+    return
+
+def search_posts(user, subreddits):
+    return
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user', type=str, help='User to be checked')
+    args = parser.parse_args()
+
+    user = args.user
+
+    generate_subreddits(user)
+
+    return
+
+if __name__ == '__main__':
+    main()

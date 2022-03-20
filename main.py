6f90506e1920860eff8e0a6e603cd527650abd78
Alec Goedinghaus <alecgoedinghaus@ucla.edu>
2022-03-20 01:15:55 -0700

    made merging more efficient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-# import requests
 import praw
 import argparse
 
@@ -7,8 +6,7 @@ client_id = os.environ['PERSONAL_USE_SCRIPT']
 client_secret = os.environ['SECRET']
 user_agent = os.environ['USER_AGENT']
 
-
-# auth = requests.auth.HTTPBasicAuth('<CLIENT_ID>', '<SECRET_TOKEN')
+DEBUG = True
 
 reddit = praw.Reddit(
     client_id=client_id,
@@ -16,69 +14,97 @@ reddit = praw.Reddit(
     user_agent=user_agent
 )
 
+def debug_print(message):
+    if DEBUG:
+        print(message)
+
 def generate_subreddits(user):
     redditor = reddit.redditor(user)
 
+    comment_subs = search_comments(redditor)
+    post_subs = search_posts(redditor)
+
+    # Choose the smaller dictionary to iterate through for efficiency
+    if len(post_subs) >= len(comment_subs):
+        subreddits = post_subs
+        subreddits_to_append = comment_subs
+    else:
+        subreddits = comment_subs
+        subreddits_to_append = post_subs
+
     # Merge both dictionaries together.
-    subreddits = search_comments(redditor) | search_posts(redditor)
+    for subreddit in subreddits_to_append:
+        if subreddit in subreddits:
+            subreddits[subreddit] += subreddits_to_append[subreddit]
+        else:
+            subreddits[subreddit] = subreddits_to_append[subreddit]
+
     return subreddits
 
 
 def search_comments(redditor):
+    debug_print('Searching through all comments')
+
     subreddits = dict()
     for comment in redditor.comments.new(limit=None):
+        name = comment.subreddit.display_name
         # If comment is in a subreddit that is already listed, increase the count,
         # otherwise create a new entry.
-        if comment.subreddit.display_name in subreddits:
-            subreddits[comment.subreddit.display_name] += 1
+        if name in subreddits:
+            subreddits[name] += 1
         else:
-            subreddits[comment.subreddit.display_name] = 1
+            subreddits[name] = 1
+
     return subreddits
 
 def search_posts(redditor):
+    debug_print('Searching through all posts')
+
     subreddits = dict()
+    for post in redditor.submissions.new(limit=None):
+        name = post.subreddit.display_name
+        # If post is in a subreddit that is already listed, increase the count,
+        # otherwise create a new entry.
+        if name in subreddits:
+            subreddits[name] += 1
+        else:
+            subreddits[name] = 1
+
     return subreddits
-
-def test_main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('user', type=str)
-    args = parser.parse_args()
-
-    user = args.user
-    search_comments(reddit.redditor(user))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('user', type=str, help='User to be checked')
-    parser.add_argument('amount', type=int, default=10, nargs='?', help='Amount of subreddits to be displayed')
-    parser.add_argument('search', type=str, default=None, nargs='?', help='Search for particular subreddit instead')
+    parser.add_argument('-a', '--amount', type=int, default=10, nargs='?', help='Amount of subreddits to be displayed')
+    parser.add_argument('-s', '--search', type=str, default=None, nargs='?', help='Search for particular subreddit instead')
+    parser.add_argument('-n', '--nsfw', type=bool, default=False, nargs='?', help='Filter for NSFW subreddits only')
     args = parser.parse_args()
 
     user = args.user
     amount = args.amount
     search = args.search
+    nsfw = args.nsfw
+
+    # MAKE SORT BY NSFW POSTS
+    # ERROR CHECK TO MAKE SURE USERNAME EXISTS
 
     # Dictionary containing {subreddit : number of interactions}
-    print('Generating subreddits')
+    debug_print('Generating subreddits')
     subreddits = generate_subreddits(user)
 
     if search != None:
         return (bool(search in subreddits))
     else:
         # Sort dictionary by most interactions
-        print('Sorting subreddits')
+        debug_print('Sorting subreddits')
         sorted_tuples = sorted(subreddits.items(), key=lambda item: item[1], reverse=True)
         sorted_subreddits = {k: v for k, v in sorted_tuples}
         subreddits_list = list(sorted_subreddits.keys())
 
         for i in range(min(len(subreddits_list), amount)):
-        # for i in range(len(subreddits_list)):
             subreddit = subreddits_list[i]
             print('{}{} interactions'.format((subreddit + ':').ljust(30), str(sorted_subreddits[subreddit]).ljust(5)))
 
-    return
-
 if __name__ == '__main__':
     main()
-    # test_main()
